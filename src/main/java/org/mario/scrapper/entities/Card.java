c1974bd756deb7877f8bb274ac3dc24371f76769
Mario <mhernandezm@opentrends.net>
2026-01-29 18:36:04 +0100

    sdfdff

--- src/main/java/org/mario/scrapper/entities/Card.java
+++ src/main/java/org/mario/scrapper/entities/Card.java
@@ -1,4 +1,4 @@
-package org.mario.scrapper.entity;
+package org.mario.scrapper.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -15,12 +15,20 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Card {
+    //Core
     @Id
     Long id;
-    String name;
-
-
     String lang;
+    Long cardmarketId;
+    String layout;
+
+    //Gameplay
+    String name;
+    List<Card> all_parts; //Si la carta pone tokens, o tiene otra mitad con meld, etc
+    List<Card> faces; //Si tiene dos caras
+    Double cmc; //Algunas cartas de unfinity tienen cmc con decimales
+    Color colorIdentity;
+
     String set;
     //Legality legalities;
 
